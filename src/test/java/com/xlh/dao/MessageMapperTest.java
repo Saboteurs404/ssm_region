cb60f0b04c8805d6b6db935e27bba7ed6c7dbded
Saboteur404 <1782824790@qq.com>
2022-06-25 20:48:45 +0800

    Changes

---
+++ src/test/java/com/xlh/dao/MessageMapperTest.java
@@ -0,0 +1,21 @@
+package com.xlh.dao;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Xlh
+ * @create 2022-06-25 20:42
+ */
+
+public class MessageMapperTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testInsert() {
+    }
+}
