be07a922982b9392d147e9608958eb7eee7d8b7f
Saboteur404 <1782824790@qq.com>
2022-07-07 21:51:07 +0800

    Changes

--- src/main/java/com/xlh/controller/userController.java
+++ src/main/java/com/xlh/controller/userController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -42,8 +44,45 @@ public class userController {
         }else{
             model.addAttribute("status",2);
         }
-        return "register"
+        return "register";
     }
+
+    @RequestMapping(value="/user/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST)
+    public String loginValidate(HttpSession session, Model model, @ModelAttribute Userinfo user) {
+        List<Userinfo> list = new ArrayList<Userinfo>();
+        Userinfo record  = new Userinfo();
+        record.setName(user.getName());
+        list = userService.selectSelective(record);
+        if (list.size() == 0) {
+            model.addAttribute("status", 1);
+        } else {
+            record.setPw(Encryption.MD5(user.getPw()));
+            list = userService.selectSelective(record);
+            if (list.size() == 0) {
+                model.addAttribute("status", 2);
+            }
+            record = list.get(0);
+            session.setAttribute("userinfo", record);
+            model.addAttribute("status", 0);
+        }
+
+        return "login";
+    }
+
+    @RequestMapping(value = "user/userinfo",method = RequestMethod.GET)
+        public String userinfo(Model model,HttpSession session){
+        Userinfo user={Userinfo} session.getAttribute("userinfo");
+        if(user!=null){
+            model.addAttribute("user",user);
+        }
+        return "userinfo";
+    }
+
 }
 
 
---
+++ src/main/webapp/WEB-INF/views/userinfo.jsp
@@ -0,0 +1,41 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xielianghui
+  Date: 2022/7/7
+  Time: 21:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>个人中心</title>
+</head>
+<body>
+<div>
+  <c:if test="${not empty user}">
+    <div>欢迎您，${user.name}${user.sex?'女士':'先生'}
+    <a href="/user/logout">注销</a> </div>
+  </c:if>
+  <c:if test="${empty user}">
+    对不起，请先<a href="/user/login">登录</a>
+  </c:if>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
